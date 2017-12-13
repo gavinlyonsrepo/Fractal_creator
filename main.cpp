261f6f6a3776e913e3a578cfcac3b3d46c72c7cd
Gavin lyons <glyons66@hotmail.com>
2017-12-13 19:29:45 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Fractal.cpp
+// Author      : Gavin Lyons
+// Version     : 1.0
+// Created on: 01-06-2017
+// Copyright   : GPL
+// Description : Fractal creator
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints !!!Hello World!!!
+	return 0;
+}
